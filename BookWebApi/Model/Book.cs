ac26c2f0ca777e0f9d34cdd4a37e0e7bfcbd6052
ahari@tavisca.in <ahari@tavisca.in>
2019-09-06 00:30:09 +0530

    added book web api and unit test cases

---
+++ BookWebApi/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWebApi.Model
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public int price { get; set; }
+        public string author { get; set; }
+    }
+}
+
